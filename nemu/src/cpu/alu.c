71fbb77d86ff0a4bfeadc02c2331bcf5f6927889
monitor <tracer@njuics.org>
2019-09-17 23:19:01 +0800

    git monitor commit.

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -2,6 +2,9 @@
 
 void set_CF_add(uint32_t result, uint32_t src, size_t data_size)
 {
+	result = sign_ext(result & (0xFFFFFFFF >> (32 - data_size)), data_size);
+	src = sign_ext(src & (0xFFFFFFFF >> (32 - data_size)), data_size);
+	cpu.eflags.CF = result < src;
 }
 
 void set_PF(uint32_t result){
@@ -20,7 +23,7 @@ void set_SF(uint32_t result, size_t data_size)
 }
 
 void set_OF(uint32_t result, uint32_t src, uint32_t dest, size_t data_size){
-	
+
 }
 
 uint32_t alu_add(uint32_t src, uint32_t dest, size_t data_size)
