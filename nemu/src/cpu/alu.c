c40ddc43d35d33af3af2641153905a313e34b3bd
monitor <tracer@njuics.org>
2019-09-19 11:21:01 +0800

    git monitor commit.

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -124,13 +124,15 @@ uint32_t alu_adc(uint32_t src, uint32_t dest, size_t data_size)
 
 uint32_t alu_sub(uint32_t src, uint32_t dest, size_t data_size)
 {
-#ifdef NEMU_REF_ALU
-	return __ref_alu_sub(src, dest, data_size);
-#else
-	printf("\e[0;31mPlease implement me at alu.c\e[0m\n");
-	assert(0);
-	return 0;
-#endif
+// #ifdef NEMU_REF_ALU
+// 	return __ref_alu_sub(src, dest, data_size);
+// #else
+// 	printf("\e[0;31mPlease implement me at alu.c\e[0m\n");
+// 	assert(0);
+// 	return 0;
+// #endif
+
+
 }
 
 uint32_t alu_sbb(uint32_t src, uint32_t dest, size_t data_size)
