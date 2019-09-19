3955504a83a8f5368bccc6746edddd8e60dabbde
tracer-pa-public <tracer@njuics.org>
2019-09-19 22:02:13 +0800

    >  clean 171870559 jzl Linux debian-pa-jzl 4.19.0-5-686-pae #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  22:02:13 up  9:23,  1 user,  load average: 0.46, 0.32, 0.28 66ffc061b82ec8304e25fba7140043fcdfbe12bb

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -429,7 +429,7 @@ uint32_t alu_sar(uint32_t src, uint32_t dest, size_t data_size)
 	cpu.eflags.CF = 0;
 	//dest = dest & ((0xffffffff) >> (32 - data_size));
 	bool nega = ((dest >> (data_size - 1) & 1) == 1);
-	dest = dest & ((0x7fffffff) >> (32 - data_size));
+	dest = dest & ((0xffffffff) >> (32 - data_size));
 	while (src > 0)
 	{
 		cpu.eflags.CF = ((dest & 1) == 1);
