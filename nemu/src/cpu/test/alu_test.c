03a6ca3715883ee580cf3ff11099143291df47f1
tracer-pa-public <tracer@njuics.org>
2019-09-19 21:58:00 +0800

    >  clean 171870559 jzl Linux debian-pa-jzl 4.19.0-5-686-pae #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  21:58:00 up  9:19,  1 user,  load average: 0.42, 0.34, 0.28 a3b36a24c58eea1b1812177de02174864ccda506

--- nemu/src/cpu/test/alu_test.c
+++ nemu/src/cpu/test/alu_test.c
@@ -42,7 +42,6 @@ typedef union {
 		: "a" (a), "c" (b)); \
 	test_eflags.val = res_eflags; \
 	res_asm = res_asm & (0xFFFFFFFF >> (32 - dataSize)); \
-	printf("res: %u,res_asm:%u\n",res,res_asm);\
 	assert(res == res_asm); \
 	assert(cpu.eflags.CF == test_eflags.CF); \
 	assert(cpu.eflags.PF == test_eflags.PF); \
