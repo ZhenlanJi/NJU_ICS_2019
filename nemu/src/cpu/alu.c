87fc73b67992738754844ed60e5f34c5d55d1090
tracer-pa-public <tracer@njuics.org>
2019-09-19 19:42:58 +0800

    >  clean 171870559 jzl Linux debian-pa-jzl 4.19.0-5-686-pae #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  19:42:58 up  7:04,  1 user,  load average: 0.21, 0.11, 0.04 6157cf80bff9ae5db1c2219af67ab651b74572c

--- nemu/src/cpu/alu.c
+++ nemu/src/cpu/alu.c
@@ -283,13 +283,22 @@ int32_t alu_imod(int64_t src, int64_t dest)
 
 uint32_t alu_and(uint32_t src, uint32_t dest, size_t data_size)
 {
-#ifdef NEMU_REF_ALU
-	return __ref_alu_and(src, dest, data_size);
-#else
-	printf("\e[0;31mPlease implement me at alu.c\e[0m\n");
-	assert(0);
-	return 0;
-#endif
+	// #ifdef NEMU_REF_ALU
+	// 	return __ref_alu_and(src, dest, data_size);
+	// #else
+	// 	printf("\e[0;31mPlease implement me at alu.c\e[0m\n");
+	// 	assert(0);
+	// 	return 0;
+	// #endif
+	uint32_t res = uint32_t res = 0;
+	res = dest & src;
+
+	set_PF(res);
+	set_ZF(res, data_size);
+	set_SF(res, data_size);
+	cpu.eflags.CF = cpu.eflags.OF = 0;
+
+	return res & (0xFFFFFFFF >> (32 - data_size));
 }
 
 uint32_t alu_xor(uint32_t src, uint32_t dest, size_t data_size)
