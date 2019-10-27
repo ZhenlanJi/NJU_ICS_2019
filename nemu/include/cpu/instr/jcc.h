dcc7e21194cef032893805c110e7dd52589f1223
monitor <tracer@njuics.org>
2019-10-27 21:34:01 +0800

    git monitor commit.

--- nemu/include/cpu/instr/jcc.h
+++ nemu/include/cpu/instr/jcc.h
@@ -2,24 +2,25 @@
 #define __INSTR_JCC_H__
 
 #define jcc_make_instr_func(suffix) \
-	make_instr_func(j, i, suffix, e) \
-	make_instr_func(j, i, suffix, a) \
-	make_instr_func(j, i, suffix, b) \
-	make_instr_func(j, i, suffix, g) \
-	make_instr_func(j, i, suffix, l) \
-	make_instr_func(j, i, suffix, o) \
-	make_instr_func(j, i, suffix, p) \
-	make_instr_func(j, i, suffix, s) \
-	make_instr_func(j, i, suffix, ae) \
-	make_instr_func(j, i, suffix, ge) \
-	make_instr_func(j, i, suffix, le) \
-	make_instr_func(j, i, suffix, na) \
-	make_instr_func(j, i, suffix, ne) \
-	make_instr_func(j, i, suffix, no) \
-	make_instr_func(j, i, suffix, np) \
-	make_instr_func(j, i, suffix, ns) \
+	make_instr_func(concat4(j, e, _, suffix)); \
+	make_instr_func(concat4(j, a, _, suffix)); \
+	make_instr_func(concat4(j, b, _, suffix)); \
+	make_instr_func(concat4(j, g, _, suffix)); \
+	make_instr_func(concat4(j, l, _, suffix)); \
+	make_instr_func(concat4(j, o, _, suffix)); \
+	make_instr_func(concat4(j, p, _, suffix)); \
+	make_instr_func(concat4(j, s, _, suffix)); \
+	make_instr_func(concat4(j, ae, _, suffix)); \
+	make_instr_func(concat4(j, ge, _, suffix)); \
+	make_instr_func(concat4(j, le, _, suffix)); \
+	make_instr_func(concat4(j, na, _, suffix)); \
+	make_instr_func(concat4(j, ne, _, suffix)); \
+	make_instr_func(concat4(j, no, _, suffix)); \
+	make_instr_func(concat4(j, np, _, suffix)); \
+	make_instr_func(concat4(j, ns, _, suffix)); \
 
 jcc_make_instr_func(near)
 jcc_make_instr_func(short_)
+make_instr_func(jecxz_short_)
 
 #endif
