0b13a7bd30e2c999197a652b8a2f1a7c39f35c17
monitor <tracer@njuics.org>
2019-10-27 21:25:01 +0800

    git monitor commit.

--- nemu/include/cpu/instr/jcc.h
+++ nemu/include/cpu/instr/jcc.h
@@ -1,6 +1,25 @@
 #ifndef __INSTR_JCC_H__
 #define __INSTR_JCC_H__
 
+#define jcc_helper(suffix) \
+	make_instr_func(j, i, suffix, e) \
+	make_instr_func(j, i, suffix, a) \
+	make_instr_func(j, i, suffix, b) \
+	make_instr_func(j, i, suffix, g) \
+	make_instr_func(j, i, suffix, l) \
+	make_instr_func(j, i, suffix, o) \
+	make_instr_func(j, i, suffix, p) \
+	make_instr_func(j, i, suffix, s) \
+	make_instr_func(j, i, suffix, ae) \
+	make_instr_func(j, i, suffix, ge) \
+	make_instr_func(j, i, suffix, le) \
+	make_instr_func(j, i, suffix, na) \
+	make_instr_func(j, i, suffix, ne) \
+	make_instr_func(j, i, suffix, no) \
+	make_instr_func(j, i, suffix, np) \
+	make_instr_func(j, i, suffix, ns) \
 
+jcc_helper(near)
+jcc_helper(short_)
 
 #endif
