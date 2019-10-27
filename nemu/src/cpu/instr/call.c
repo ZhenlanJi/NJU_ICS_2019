79fc5a5ec6ce42ac43d92c8114f4fe6273d77c4a
monitor <tracer@njuics.org>
2019-10-27 19:39:01 +0800

    git monitor commit.

--- nemu/src/cpu/instr/call.c
+++ nemu/src/cpu/instr/call.c
@@ -13,16 +13,17 @@ make_instr_func(call_near)
     operand_read(&rel);
     int offset = sign_ext(rel.val, data_size);
 
-    cpu.eip-=dest_size;
-    
+    cpu.eip -= dest_size;
+
     OPERAND temp_dest;
     temp_dest.addr = cpu.esp;
     temp_dest.sreg = SREG_SS;
     temp_dest.data_size = data_size;
     temp_dest.type = OPR_MEM;
-    temp_dest.val = sign_ext(opr_src.val, data_size);
+    temp_dest.val = cpu.eip + 1 + dest_size;
+    operand_write(&temp_dest);
 
     cpu.eip += offset;
 
-    return 1 + data_size / 8;
+    return 1 + dest_size;
 }
