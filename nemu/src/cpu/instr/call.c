94e2c3b06716bd8e35d3a690a82f122cb42875a2
monitor <tracer@njuics.org>
2019-10-27 19:25:01 +0800

    git monitor commit.

--- nemu/src/cpu/instr/call.c
+++ nemu/src/cpu/instr/call.c
@@ -1,1 +1,23 @@
 #include "cpu/instr.h"
+
+make_instr_func(call_near)
+{
+        uint32_t dest_size = data_size / 8;
+    assert(dest_size == 1 || dest_size == 2 || dest_size == 4);
+        
+        OPERAND rel;
+        rel.type = OPR_IMM;
+        rel.sreg = SREG_CS;
+        rel.data_size = data_size;
+        rel.addr = eip + 1;
+
+        operand_read(&rel);
+
+        int offset = sign_ext(rel.val, data_size);
+        // thank Ting Xu from CS'17 for finding this bug
+        print_asm_1("jmp", "", 1 + data_size / 8, &rel);
+
+        cpu.eip += offset;
+
+        return 1 + data_size / 8;
+}
