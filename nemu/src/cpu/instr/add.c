c82b84179c0797dc55cd267e7446145e47943483
monitor <tracer@njuics.org>
2019-10-27 20:30:01 +0800

    git monitor commit.

--- nemu/src/cpu/instr/add.c
+++ nemu/src/cpu/instr/add.c
@@ -1,1 +1,11 @@
 #include "cpu/instr.h"
+
+static void instr_execute_2op()
+{
+    operand_read(&opr_src);
+    operand_read(&opr_dest);
+    opr_src.val = sign_ext(opr_src.val, data_size);
+    opr_dest.val = sign_ext(opr_dest.val, data_size);
+    alu_add(opr_src.val, opr_dest.val, data_size);
+    operand_write(&opr_dest);
+}
