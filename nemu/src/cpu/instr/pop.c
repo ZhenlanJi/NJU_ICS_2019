83256fcc837e8923c49c307ddd24b80c018dfd56
monitor <tracer@njuics.org>
2019-10-29 23:51:01 +0800

    git monitor commit.

--- nemu/src/cpu/instr/pop.c
+++ nemu/src/cpu/instr/pop.c
@@ -2,17 +2,19 @@
 
 static void instr_execute_1op()
 {
+    int writing_size = data_size / 8;
+    assert(writing_size == 1 || writing_size == 2 || writing_size == 4);
+
     OPERAND opr_obj;
-    int writing_size=data_size/8;
-    assert(writing_size==1||writing_size==2||writing_size==4);
-    opr_obj.data_size=data_size;
-    opr_obj.addr=cpu.esp;
-    opr_obj.sreg=SREG_SS;
-    opr_obj.type=OPR_MEM;
+    opr_obj.data_size = data_size;
+    opr_obj.addr = cpu.esp;
+    opr_obj.sreg = SREG_SS;
+    opr_obj.type = OPR_MEM;
     operand_read(&opr_obj);
-    opr_src.val=opr_obj.val;
+
+    opr_src.val = opr_obj.val;
     operand_write(&opr_src);
-    cpu.esp+=writing_size;
+    cpu.esp += writing_size;
 }
 
 make_instr_impl_1op(pop, r, v)
