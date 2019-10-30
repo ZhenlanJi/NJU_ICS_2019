791cf615da1a1a15c816368585097bcc76fcb215
tracer-pa-public <tracer@njuics.org>
2019-10-30 20:13:14 +0800

    >  nemu 171870559 jzl Linux debian-pa-jzl 4.19.0-5-686-pae #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  20:13:14 up  4:51,  1 user,  load average: 0.66, 0.34, 0.14 2f74a91f74cf6f4255dd29a42269fcde786c179

--- nemu/src/cpu/decode/opcode.c
+++ nemu/src/cpu/decode/opcode.c
@@ -43,7 +43,7 @@ instr_func opcode_entry[256] = {
     /* 0x9c - 0x9f*/ inv, inv, sahf, inv,
     /* 0xa0 - 0xa3*/ mov_o2a_b, mov_o2a_v, mov_a2o_b, mov_a2o_v,
     /* 0xa4 - 0xa7*/ movs_b, movs_v, inv, inv,
-    /* 0xa8 - 0xab*/ inv, inv, inv, inv,
+    /* 0xa8 - 0xab*/ test_i2a_b, test_i2a_v, inv, inv,
     /* 0xac - 0xaf*/ inv, inv, inv, inv,
     /* 0xb0 - 0xb3*/ mov_i2r_b, mov_i2r_b, mov_i2r_b, mov_i2r_b,
     /* 0xb4 - 0xb7*/ mov_i2r_b, mov_i2r_b, mov_i2r_b, mov_i2r_b,
@@ -105,11 +105,11 @@ instr_func group_2_cv_entry[8] =
 
 /* 0xf6 */
 instr_func group_3_b_entry[8] =
-    {inv, inv, not_rm_b, inv, mul_rm2a_b, imul_rm2a_b, inv, idiv_rm2a_b};
+    {test_r2rm_b, inv, not_rm_b, inv, mul_rm2a_b, imul_rm2a_b, inv, idiv_rm2a_b};
 
 /* 0xf7 */
 instr_func group_3_v_entry[8] =
-    {inv, inv, not_rm_v, inv, mul_rm2a_v, imul_rm2a_v, inv, idiv_rm2a_v};
+    {test_i2rm_v, inv, not_rm_v, inv, mul_rm2a_v, imul_rm2a_v, inv, idiv_rm2a_v};
 
 /* 0xff */
 instr_func group_5_indirect_entry[8] =
