b93a6b84ef22b6a1a27ff76032fbcc052d83c2ea
monitor <tracer@njuics.org>
2019-10-30 13:22:01 +0800

    git monitor commit.

--- nemu/include/cpu/instr/imul.h
+++ nemu/include/cpu/instr/imul.h
@@ -2,7 +2,9 @@
 #define __INSTR_IMUL_H__
 
 make_instr_func(imul_rm2r_v);
-
-
+make_instr_func(imul_rm2a_b);
+make_instr_func(imul_rm2a_v);
+make_instr_func(imul_irm2r_v);
+make_instr_func(imul_i8rm2r_v);
 
 #endif
