f8c2e4c12d3a22a02032d23bb8eb2e9911ee4eb4
monitor <tracer@njuics.org>
2019-10-30 19:19:01 +0800

    git monitor commit.

--- nemu/include/cpu/instr/idiv.h
+++ nemu/include/cpu/instr/idiv.h
@@ -1,10 +1,7 @@
 #ifndef __INSTR_IDIV_H__
 #define __INSTR_IDIV_H__
 
-make_instr_func(imul_rm2r_v);
-make_instr_func(imul_rm2a_b);
-make_instr_func(imul_rm2a_v);
-make_instr_func(imul_irm2r_v);
-make_instr_func(imul_i8rm2r_v);
+make_instr_func(idiv_rm2a_b);
+make_instr_func(idiv_rm2a_v);
 
 #endif
