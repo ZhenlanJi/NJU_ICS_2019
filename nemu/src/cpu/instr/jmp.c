de28aeb9663e984ecc6723af9850a958a014bad1
monitor <tracer@njuics.org>
2019-12-03 22:07:01 +0800

    git monitor commit.

--- nemu/src/cpu/instr/jmp.c
+++ nemu/src/cpu/instr/jmp.c
@@ -2,51 +2,64 @@
 
 make_instr_func(jmp_near)
 {
-        OPERAND rel;
-        rel.type = OPR_IMM;
-        rel.sreg = SREG_CS;
-        rel.data_size = data_size;
-        rel.addr = eip + 1;
+    OPERAND rel;
+    rel.type = OPR_IMM;
+    rel.sreg = SREG_CS;
+    rel.data_size = data_size;
+    rel.addr = eip + 1;
 
-        operand_read(&rel);
+    operand_read(&rel);
 
-        int offset = sign_ext(rel.val, data_size);
-        // thank Ting Xu from CS'17 for finding this bug
-        print_asm_1("jmp", "", 1 + data_size / 8, &rel);
+    int offset = sign_ext(rel.val, data_size);
+    // thank Ting Xu from CS'17 for finding this bug
+    print_asm_1("jmp", "", 1 + data_size / 8, &rel);
 
-        cpu.eip += offset;
+    cpu.eip += offset;
 
-        return 1 + data_size / 8;
+    return 1 + data_size / 8;
 }
 
 make_instr_func(jmp_short)
 {
-        OPERAND rel;
-        rel.type = OPR_IMM;
-        rel.sreg = SREG_CS;
-        rel.data_size = 8;
-        rel.addr = eip + 1;
+    OPERAND rel;
+    rel.type = OPR_IMM;
+    rel.sreg = SREG_CS;
+    rel.data_size = 8;
+    rel.addr = eip + 1;
 
-        operand_read(&rel);
+    operand_read(&rel);
 
-        int offset = sign_ext(rel.val, 8);
-        // thank Ting Xu from CS'17 for finding this bug
-        //print_asm_1("jmp", "", 1 + data_size / 8, &rel);
+    int offset = sign_ext(rel.val, 8);
+    // thank Ting Xu from CS'17 for finding this bug
+    //print_asm_1("jmp", "", 1 + data_size / 8, &rel);
 
-        cpu.eip += offset;
+    cpu.eip += offset;
 
-        return 2;
+    return 2;
 }
 
 make_instr_func(jmp_near_indirect)
 {
-        OPERAND rel;
-        rel.data_size = data_size;
-        int len = 1;
-        len += modrm_rm(cpu.eip + 1, &rel);
-        operand_read(&rel);
+    OPERAND rel;
+    rel.data_size = data_size;
+    int len = 1;
+    len += modrm_rm(cpu.eip + 1, &rel);
+    operand_read(&rel);
 
-        int offset = sign_ext(rel.val, data_size);
-        cpu.eip = offset;
-        return 0;
+    int offset = sign_ext(rel.val, data_size);
+    cpu.eip = offset;
+    return 0;
+}
+
+make_instr_func(jmp_far_imm)
+{
+    OPERAND rel;
+    rel.type = OPR_IMM;
+    rel.sreg = SREG_CS;
+    rel.data_size = data_size;
+    rel.addr = eip + 1;
+    operand_read(&rel);
+
+    int offset = sign_ext(rel.value, data_size);
+    cpu.eip = offset;
 }
