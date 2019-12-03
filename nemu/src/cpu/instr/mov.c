982ebcfbc1ebb4a58ad495c2468dcfc07f431a5b
monitor <tracer@njuics.org>
2019-12-03 21:43:01 +0800

    git monitor commit.

--- nemu/src/cpu/instr/mov.c
+++ nemu/src/cpu/instr/mov.c
@@ -1,10 +1,10 @@
 #include "cpu/instr.h"
 
-static void instr_execute_2op() 
+static void instr_execute_2op()
 {
-	operand_read(&opr_src);
-	opr_dest.val = opr_src.val;
-	operand_write(&opr_dest);
+    operand_read(&opr_src);
+    opr_dest.val = opr_src.val;
+    operand_write(&opr_dest);
 }
 
 make_instr_impl_2op(mov, r, rm, b)
@@ -20,59 +20,67 @@ make_instr_impl_2op(mov, a, o, v)
 make_instr_impl_2op(mov, o, a, b)
 make_instr_impl_2op(mov, o, a, v)
 
-make_instr_func(mov_zrm82r_v) {
-	int len = 1;
-	OPERAND r, rm;
-	r.data_size = data_size;
-	rm.data_size = 8;
-	len += modrm_r_rm(eip + 1, &r, &rm);
-	
-	operand_read(&rm);
-	r.val = rm.val;
-	operand_write(&r);
+make_instr_func(mov_zrm82r_v)
+{
+    int len = 1;
+    OPERAND r, rm;
+    r.data_size = data_size;
+    rm.data_size = 8;
+    len += modrm_r_rm(eip + 1, &r, &rm);
 
-	print_asm_2("mov", "", len, &rm, &r);
-	return len;
+    operand_read(&rm);
+    r.val = rm.val;
+    operand_write(&r);
+
+    print_asm_2("mov", "", len, &rm, &r);
+    return len;
 }
 
-make_instr_func(mov_zrm162r_l) {
-        int len = 1;
-        OPERAND r, rm;
-        r.data_size = 32;
-        rm.data_size = 16;
-        len += modrm_r_rm(eip + 1, &r, &rm);
+make_instr_func(mov_zrm162r_l)
+{
+    int len = 1;
+    OPERAND r, rm;
+    r.data_size = 32;
+    rm.data_size = 16;
+    len += modrm_r_rm(eip + 1, &r, &rm);
 
-        operand_read(&rm);
-        r.val = rm.val;
-        operand_write(&r);
-	print_asm_2("mov", "", len, &rm, &r);
-        return len;
+    operand_read(&rm);
+    r.val = rm.val;
+    operand_write(&r);
+    print_asm_2("mov", "", len, &rm, &r);
+    return len;
 }
 
-make_instr_func(mov_srm82r_v) {
-        int len = 1;
-        OPERAND r, rm;
-        r.data_size = data_size;
-        rm.data_size = 8;
-        len += modrm_r_rm(eip + 1, &r, &rm);
-        
-	operand_read(&rm);
-        r.val = sign_ext(rm.val, 8);
-        operand_write(&r);
-	print_asm_2("mov", "", len, &rm, &r);
-        return len;
+make_instr_func(mov_srm82r_v)
+{
+    int len = 1;
+    OPERAND r, rm;
+    r.data_size = data_size;
+    rm.data_size = 8;
+    len += modrm_r_rm(eip + 1, &r, &rm);
+
+    operand_read(&rm);
+    r.val = sign_ext(rm.val, 8);
+    operand_write(&r);
+    print_asm_2("mov", "", len, &rm, &r);
+    return len;
 }
 
-make_instr_func(mov_srm162r_l) {
-        int len = 1;
-        OPERAND r, rm;
-        r.data_size = 32;
-        rm.data_size = 16;
-        len += modrm_r_rm(eip + 1, &r, &rm);
-        operand_read(&rm);
-        r.val = sign_ext(rm.val, 16);
-        operand_write(&r);
+make_instr_func(mov_srm162r_l)
+{
+    int len = 1;
+    OPERAND r, rm;
+    r.data_size = 32;
+    rm.data_size = 16;
+    len += modrm_r_rm(eip + 1, &r, &rm);
+    operand_read(&rm);
+    r.val = sign_ext(rm.val, 16);
+    operand_write(&r);
 
-	print_asm_2("mov", "", len, &rm, &r);
-        return len;
+    print_asm_2("mov", "", len, &rm, &r);
+    return len;
+}
+
+make_instr_func(mov_c2r_l)
+{
 }
