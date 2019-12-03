a47f26cd59fea0dc85806eeabf7656de192e58a9
monitor <tracer@njuics.org>
2019-12-03 21:38:01 +0800

    git monitor commit.

--- nemu/src/cpu/instr/lgdt.c
+++ nemu/src/cpu/instr/lgdt.c
@@ -5,8 +5,15 @@ make_instr_func(lgdt)
     int len = 1;
     OPERAND rel;
     len += modrm_rm(eip + 1, &rel);
-    rel.data_size = 16;
 
+    rel.data_size = 16;
     operand_read(&rel);
-    cpu.gdtr.limit=rel.val
+    cpu.gdtr.limit = rel.val;
+
+    rel.data_size = 32;
+    rel.addr += 2;
+    operand_read(&rel);
+    cpu.gdtr.base = rel.val;
+
+    return len;
 }
