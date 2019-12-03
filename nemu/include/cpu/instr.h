9903087b16c48c7b666e757c0b09046e65818c1c
monitor <tracer@njuics.org>
2019-12-03 18:14:01 +0800

    git monitor commit.

--- nemu/include/cpu/instr.h
+++ nemu/include/cpu/instr.h
@@ -47,6 +47,7 @@ extern uint8_t data_size;
 #include "cpu/instr/stos.h"
 #include "cpu/instr/cmps.h"
 #include "cpu/instr/std.h"
+#include "cpu/instr/lgtd.h"
 
 /* TODO: add more instructions here */
 
