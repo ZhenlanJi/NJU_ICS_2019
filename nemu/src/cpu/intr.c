22650ad9b774249992b77b9860866b81b6c0f6e7
tracer-pa-public <tracer@njuics.org>
2020-01-18 23:31:36 +0800

    >  clean 171870559 jzl Linux debian-pa-jzl 4.19.0-5-686-pae #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  23:31:36 up  9:15,  1 user,  load average: 0.16, 0.17, 0.23 904b6fc76dbd8e72c6f15580d5e5573f535b422b

--- nemu/src/cpu/intr.c
+++ nemu/src/cpu/intr.c
@@ -7,7 +7,7 @@ void raise_intr(uint8_t intr_no)
 #ifdef IA32_INTR
 	// printf("Please implement raise_intr()");
 	// assert(0);
-	printf("intr_no: %u", intr_no);
+	//printf("intr_no: %u", intr_no);
 	cpu.esp -= 4;
 	vaddr_write(cpu.esp, SREG_SS, 4, cpu.eflags.val);
 	cpu.esp -= 2;
@@ -15,7 +15,7 @@ void raise_intr(uint8_t intr_no)
 	cpu.esp -= 4;
 	vaddr_write(cpu.esp, SREG_SS, 4, cpu.eip);
 
-	laddr_t l_addr = cpu.idtr.base + 8 * intr_no;
+	laddr_t l_addr = cpu.idtr.base + sizeof(GateDesc) * intr_no;
 	GateDesc gatedesc;
 	gatedesc.val[1] = laddr_read(l_addr + 4, 4);
 	gatedesc.val[0] = laddr_read(l_addr, 4);
@@ -24,7 +24,8 @@ void raise_intr(uint8_t intr_no)
 
 	if (gatedesc.type == 0xe)
 		cpu.eflags.IF = 0;
-
+	
+	cpu.cs.val = gatedesc.selector;
 	cpu.eip = (gatedesc.offset_31_16 << 16) + gatedesc.offset_15_0;
 #endif
 }
