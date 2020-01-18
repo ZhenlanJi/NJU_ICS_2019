3825175a454184ba2b909f0eddcdfb3f6fcac769
tracer-pa-public <tracer@njuics.org>
2020-01-18 23:16:31 +0800

    >  clean 171870559 jzl Linux debian-pa-jzl 4.19.0-5-686-pae #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  23:16:31 up  9:00,  1 user,  load average: 0.34, 0.41, 0.28 22add858f54a2358dcc3fdc710ba596d547fec77

--- nemu/src/cpu/instr/lidt.c
+++ nemu/src/cpu/instr/lidt.c
@@ -2,36 +2,36 @@
 
 make_instr_func(lidt)
 {
-    // int len = 1;
-    // OPERAND limit, base;
-
-    // limit.type = OPR_IMM;
-    // limit.data_size = 16;
-    // limit.addr = eip + 1;
-    // len += modrm_rm(eip + 1, &limit);
-    // operand_read(&limit);
-    // cpu.idtr.limit = limit.val;
-
-    // base.type = OPR_IMM;
-    // base.data_size = 32;
-    // base.addr = limit.addr + 2;
-    // operand_read(&base);
-    // cpu.idtr.base = base.val;
-
-    // return 6;
-
     int len = 1;
-    OPERAND rel;
-    len += modrm_rm(eip + 1, &rel);
+    OPERAND limit, base;
 
-    rel.data_size = 16;
-    operand_read(&rel);
-    cpu.idtr.limit = rel.val;
+    limit.type = OPR_IMM;
+    limit.data_size = 16;
+    limit.addr = eip + 1;
+    len += modrm_rm(eip + 1, &limit);
+    operand_read(&limit);
+    cpu.idtr.limit = limit.val;
 
-    rel.data_size = 32;
-    rel.addr += 2;
-    operand_read(&rel);
-    cpu.idtr.base = rel.val;
+    base.type = OPR_IMM;
+    base.data_size = 32;
+    base.addr = limit.addr + 2;
+    operand_read(&base);
+    cpu.idtr.base = base.val;
 
-    return len;
+    return 6;
+
+    // int len = 1;
+    // OPERAND rel;
+    // len += modrm_rm(eip + 1, &rel);
+
+    // rel.data_size = 16;
+    // operand_read(&rel);
+    // cpu.idtr.limit = rel.val;
+
+    // rel.data_size = 32;
+    // rel.addr += 2;
+    // operand_read(&rel);
+    // cpu.idtr.base = rel.val;
+
+    // return len;
 }
