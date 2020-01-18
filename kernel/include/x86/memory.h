b01d61007923cd0242d6559ad45b4bc7b70e379b
monitor <tracer@njuics.org>
2020-01-19 01:28:01 +0800

    git monitor commit.

--- kernel/include/x86/memory.h
+++ kernel/include/x86/memory.h
@@ -108,7 +108,7 @@ typedef struct TrapFrame
 {
 	uint32_t edi, esi, ebp, xxx, ebx, edx, ecx, eax; // GPRs
 	int32_t irq;									 // #irq
-	//uint32_t error_code;							 // error code
+	uint32_t error_code;							 // error code
 	uint32_t eip, cs, eflags;						 // execution state saved by hardware
 } TrapFrame;
 
