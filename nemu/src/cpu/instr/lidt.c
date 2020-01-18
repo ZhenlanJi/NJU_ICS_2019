3b9e97f663aadb6d874356015351e2cba78a1fc9
tracer-pa-public <tracer@njuics.org>
2020-01-18 23:13:34 +0800

    >  nemu 171870559 jzl Linux debian-pa-jzl 4.19.0-5-686-pae #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  23:13:34 up  8:57,  1 user,  load average: 0.23, 0.29, 0.22 36bce05fa96c83099e6baab87ca82e35ad6e47c2

--- nemu/src/cpu/instr/lidt.c
+++ nemu/src/cpu/instr/lidt.c
@@ -19,4 +19,19 @@ make_instr_func(lidt)
     // cpu.idtr.base = base.val;
 
     // return 6;
+
+    int len = 1;
+    OPERAND rel;
+    len += modrm_rm(eip + 1, &rel);
+
+    rel.data_size = 16;
+    operand_read(&rel);
+    cpu.idtr.limit = rel.val;
+
+    rel.data_size = 32;
+    rel.addr += 2;
+    operand_read(&rel);
+    cpu.idtr.base = rel.val;
+
+    return len;
 }
