5a3d31c2036a1d48f99c1bb0e6fe70d43ee0731a
tracer-pa-public <tracer@njuics.org>
2020-01-18 23:12:56 +0800

    >  clean 171870559 jzl Linux debian-pa-jzl 4.19.0-5-686-pae #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  23:12:56 up  8:56,  1 user,  load average: 0.20, 0.30, 0.22 e98fe1f6132ff261aa9b9514cc86ca2edc582f81

--- nemu/src/cpu/instr/lidt.c
+++ nemu/src/cpu/instr/lidt.c
@@ -2,21 +2,21 @@
 
 make_instr_func(lidt)
 {
-    int len = 1;
-    OPERAND limit, base;
+    // int len = 1;
+    // OPERAND limit, base;
 
-    limit.type = OPR_IMM;
-    limit.data_size = 16;
-    limit.addr = eip + 1;
-    len += modrm_rm(eip + 1, &limit);
-    operand_read(&limit);
-    cpu.idtr.limit = limit.val;
+    // limit.type = OPR_IMM;
+    // limit.data_size = 16;
+    // limit.addr = eip + 1;
+    // len += modrm_rm(eip + 1, &limit);
+    // operand_read(&limit);
+    // cpu.idtr.limit = limit.val;
 
-    base.type = OPR_IMM;
-    base.data_size = 32;
-    base.addr = limit.addr + 2;
-    operand_read(&base);
-    cpu.idtr.base = base.val;
+    // base.type = OPR_IMM;
+    // base.data_size = 32;
+    // base.addr = limit.addr + 2;
+    // operand_read(&base);
+    // cpu.idtr.base = base.val;
 
-    return 6;
+    // return 6;
 }
