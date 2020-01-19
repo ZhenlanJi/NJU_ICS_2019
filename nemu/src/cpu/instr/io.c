e94c838b929954420a3e3497fbdbdd57d185ca8a
monitor <tracer@njuics.org>
2020-01-19 18:52:01 +0800

    git monitor commit.

--- nemu/src/cpu/instr/io.c
+++ nemu/src/cpu/instr/io.c
@@ -1,3 +1,20 @@
 #include "cpu/instr.h"
 #include "device/port_io.h"
 
+make_instr_func(in_b)
+{
+    cpu.eax = pio_read((uint16_t)cpu.edx, 1);
+    return 1;
+}
+
+make_instr_func(in_v)
+{
+    cpu.eax = pio_read((uint16_t)cpu.edx, data_size / 8);
+    return 1;
+}
+
+make_instr_func(out_b)
+{
+    pio_write((uint16_t)cpu.edx, 1, cpu.eax);
+    return 1;
+}
