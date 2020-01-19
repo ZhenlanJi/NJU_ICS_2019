bcde9b2024cf0b6ae7632450e231f6e09c9cfa82
tracer-pa-public <tracer@njuics.org>
2020-01-19 22:17:08 +0800

    >  clean 171870559 jzl Linux debian-pa-jzl 4.19.0-5-686-pae #1 SMP Debian 4.19.37-5+deb10u2 (2019-08-08) i686 GNU/Linux  22:17:08 up 11:27,  1 user,  load average: 0.10, 0.07, 0.09 157917a65e6553e77665637b54dcf7d479d32

--- kernel/src/elf/elf.c
+++ kernel/src/elf/elf.c
@@ -47,7 +47,7 @@ uint32_t loader()
 //Log("%x",p_vaddr);
 /* TODO: copy the segment from the ELF file to its proper memory area */
 #ifdef HAS_DEVICE_IDE
-			ide_read((uint8_t*)p_vaddr, ph->p_offset, ph->p_filesz)
+			ide_read((uint8_t *)p_vaddr, ph->p_offset, ph->p_filesz);
 
 #else
 
